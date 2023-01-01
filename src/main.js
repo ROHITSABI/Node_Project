63551f3867b87362527a6f42387c798bc580cc87
Rohit <rsabi2708@gmail.com>
2023-01-01 12:30:37 +0530

    reading-file

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import { sum } from "./mymath.js";
-import { mul } from "./mymath.js";
+// import { sum } from "./mymath.js";
+// import { mul } from "./mymath.js";
 
-function main() {
-  let output = sum(1, 1);
-  console.log("Sum", output);
-  let output1 = mul(2, 4);
-  console.log("Mul", output1);
+import { readFile } from "node:fs/promises";
+async function main() {
+  console.log("Reading File Path");
+  let filepath = "D:Web-Pgmg-TechnologiesWeb_Html_Pgmgday_2\1.html";
+  let output = await readFile(filepath, { encoding: "utf-8" });
+  console.log(output);
 }
 main();
